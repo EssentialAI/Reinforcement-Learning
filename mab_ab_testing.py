fd212391a9ba61702b625f2c9b8e004ccf7aac78
Naresh <naresh017edu@gmail.com>
2022-04-21 00:09:29 +0530

    completed mab code to work for gaussian and bernoulli with multiple epsilon values

--- mab_ab_testing.py
+++ mab_ab_testing.py
@@ -26,24 +26,31 @@ class GaussianBandit(object):
         return np.round(reward, 1)
     
 class Agent(object):
-    def __init__(self, bandits, n_train=10000, n_test = 90000, band_type = 'gaussian'):
+    def __init__(self, bandits, train_test_split=0.1, num_trails = 10000, band_type = 'gaussian'):
         
         if band_type=='gaussian':
             self.bandits = [GaussianBandit(bandit[0], bandit[1]) for bandit in bandits]
+        
         else:
             self.bandits = [BernoulliBandit(bandit) for bandit in bandits]
             
         self.n_ads = len(bandits)
+        self.n_train = int(num_trails*train_test_split)
+        self.n_test = int(num_trails*(1-train_test_split))
+        self.reward_dict = {}
+        self.n_trails = num_trails
+            
+    def reset_agent(self):
         self.total_reward=0
         self.N = np.zeros(self.n_ads)
         self.Q = np.zeros(self.n_ads)
         self.avg_rewards=[]
-        self.n_train = n_train
-        self.n_test = n_test
-        
-        self.eps = 0.1
 
-    def train(self):
+    def best_ad_random_actions(self):
+        
+        self.reset_agent()
+        print("Agent is reset")
+        
         for i in range(self.n_train):
             ad_chosen = np.random.randint(self.n_ads)
             R = self.bandits[ad_chosen].get_reward()  # Observe reward
@@ -56,10 +63,9 @@ class Agent(object):
         best_ad_index = np.argmax(self.Q)
         print(self.Q)
         
-        print("After training the best performing ad is {}".format(best_ad_index+1))
-        return best_ad_index
-    
-    def test(self, best_ad_index):
+        print("Best performing ad after random actions is {}. Steps used to train: {}".format(best_ad_index+1, self.n_train))
+        
+        print("Using optimal ad for rest {} steps".format(self.n_test))
         for i in range(self.n_test):
             R = self.bandits[best_ad_index].get_reward()
             self.total_reward += R
@@ -67,21 +73,24 @@ class Agent(object):
             self.avg_rewards.append(avg_reward_so_far)
         
         df = pd.DataFrame(self.avg_rewards, columns = ['A/B/n'])
-        return df
+        return df, best_ad_index
     
-    def epsilon(self):
+    def epsilon_greedy(self, eps):
+        
+        self.reset_agent()
+        print("Agent is reset")
+        
         ad_chosen = np.random.randint(self.n_ads)
         
-        for i in range(self.n_test):
+        for i in range(self.n_trails):
             R = self.bandits[ad_chosen].get_reward()
-            R = self.bandits[ad_chosen].get_reward()  # Observe reward
             self.N[ad_chosen] += 1
             self.Q[ad_chosen] += (1 / self.N[ad_chosen]) * (R - self.Q[ad_chosen])
             self.total_reward += R
             avg_reward_so_far = self.total_reward / (i + 1)
             self.avg_rewards.append(avg_reward_so_far)
             
-            if np.random.uniform()<= self.eps:
+            if np.random.uniform()<= eps:
                 ad_chosen = np.random.randint(self.n_ads)
             else:
                 ad_chosen = np.argmax(self.Q)
@@ -89,35 +98,43 @@ class Agent(object):
         best_ad_index = np.argmax(self.Q)
         print(self.Q)
         
-        print("After epsilon the best performing ad is {}".format(best_ad_index+1))
+        print("After epsilon-greedy method the best performing ad is {}. Epsilon: {}".format(best_ad_index+1, eps))
         
-        df = pd.DataFrame(self.avg_rewards, columns = ['A/B/n'])
-        return df
+        self.reward_dict[eps] = self.avg_rewards
+        
+        df= pd.DataFrame.from_dict(self.reward_dict)
+        return df, best_ad_index
     
-    def plot(self, df):
-        df['A/B/n'].plot()
-        average_reward=np.round(sum(self.avg_rewards)/(self.n_train+self.n_test), 3)
-        plt.title("Average reward is {}".format(average_reward))
+    def plot(self, df, mode = 'random agent'):
+        df.plot()
+        if mode == 'random agent' or mode == 'single epsilon':
+            plt.title("Average reward is {}".format(df.values[-1]))
+    
+        elif  mode == 'multiple epsilons':
+            plt.xscale('log')
+            
         plt.xlabel("Number of Impressions")
         plt.ylabel("Reward")
         plt.show()
+            
 
 
 if __name__ =="__main__":
     
     # Gaussian Bandits
-    # ads = [(3,2), (0,1), (-1,6), (1,4)]
+    ads = [(3,2), (0,1), (-1,6), (1,4)]
     
     # Bernoulli Bandits
-    ads = [0.004, 0.016, 0.2, 0.028, 0.031]
+    # ads = [0.004, 0.016, 0.2, 0.028, 0.031]
     
-    def random_agent():
-        ads_initialized = Agent(ads, band_type="gaussian1")
-        best_ad=ads_initialized.train()
-        df = ads_initialized.test(best_ad)
-        ads_initialized.plot(df)
+    eps_values = [0.01, 0.1, 0.2, 0.6]
         
-    def epsilon_greedy_agent():
-        ads_initialized = Agent(ads, band_type="bernoulli")
-        df = ads_initialized.epsilon()
-        ads_initialized.plot(df)
+    ads_initialized = Agent(ads, band_type="gaussian")
+    
+    # rewards, best_ad = ads_initialized.epsilon_greedy(0.01)
+    # ads_initialized.plot(rewards, mode = 'single epsilon')
+    
+    for eps in eps_values:
+        rewards, best_ad = ads_initialized.epsilon_greedy(eps)
+        
+    ads_initialized.plot(rewards, mode = 'multiple epsilons')
