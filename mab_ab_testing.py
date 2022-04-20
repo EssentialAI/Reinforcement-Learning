483dbad56155da21e88fdaf32943640ce08b8c9c
Naresh <naresh017edu@gmail.com>
2022-04-20 19:53:13 +0530

    finished epsilon greedy code for both gaussian and bernoulli rewards

--- mab_ab_testing.py
+++ mab_ab_testing.py
@@ -25,7 +25,7 @@ class GaussianBandit(object):
         reward = np.random.normal(self.mean, self.stdev)
         return np.round(reward, 1)
     
-class Game(object):
+class Agent(object):
     def __init__(self, bandits, n_train=10000, n_test = 90000, band_type = 'gaussian'):
         
         if band_type=='gaussian':
@@ -40,6 +40,8 @@ class Game(object):
         self.avg_rewards=[]
         self.n_train = n_train
         self.n_test = n_test
+        
+        self.eps = 0.1
 
     def train(self):
         for i in range(self.n_train):
@@ -67,12 +69,39 @@ class Game(object):
         df = pd.DataFrame(self.avg_rewards, columns = ['A/B/n'])
         return df
     
+    def epsilon(self):
+        ad_chosen = np.random.randint(self.n_ads)
+        
+        for i in range(self.n_test):
+            R = self.bandits[ad_chosen].get_reward()
+            R = self.bandits[ad_chosen].get_reward()  # Observe reward
+            self.N[ad_chosen] += 1
+            self.Q[ad_chosen] += (1 / self.N[ad_chosen]) * (R - self.Q[ad_chosen])
+            self.total_reward += R
+            avg_reward_so_far = self.total_reward / (i + 1)
+            self.avg_rewards.append(avg_reward_so_far)
+            
+            if np.random.uniform()<= self.eps:
+                ad_chosen = np.random.randint(self.n_ads)
+            else:
+                ad_chosen = np.argmax(self.Q)
+        
+        best_ad_index = np.argmax(self.Q)
+        print(self.Q)
+        
+        print("After epsilon the best performing ad is {}".format(best_ad_index+1))
+        
+        df = pd.DataFrame(self.avg_rewards, columns = ['A/B/n'])
+        return df
+    
     def plot(self, df):
         df['A/B/n'].plot()
         average_reward=np.round(sum(self.avg_rewards)/(self.n_train+self.n_test), 3)
         plt.title("Average reward is {}".format(average_reward))
         plt.xlabel("Number of Impressions")
         plt.ylabel("Reward")
+        plt.show()
+
 
 if __name__ =="__main__":
     
@@ -81,7 +110,14 @@ if __name__ =="__main__":
     
     # Bernoulli Bandits
     ads = [0.004, 0.016, 0.2, 0.028, 0.031]
-    ads_initialized = Game(ads, band_type="gaussian1")
-    best_ad=ads_initialized.train()
-    df = ads_initialized.test(best_ad)
-    ads_initialized.plot(df)
+    
+    def random_agent():
+        ads_initialized = Agent(ads, band_type="gaussian1")
+        best_ad=ads_initialized.train()
+        df = ads_initialized.test(best_ad)
+        ads_initialized.plot(df)
+        
+    def epsilon_greedy_agent():
+        ads_initialized = Agent(ads, band_type="bernoulli")
+        df = ads_initialized.epsilon()
+        ads_initialized.plot(df)
