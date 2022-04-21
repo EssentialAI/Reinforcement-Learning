2fd88c839ace792f84e65b760c4c3fe19b990309
Naresh <naresh017edu@gmail.com>
2022-04-21 11:51:18 +0530

    added optimistic initial values code

--- mab_ab_testing.py
+++ mab_ab_testing.py
@@ -26,7 +26,7 @@ class GaussianBandit(object):
         return np.round(reward, 1)
     
 class Agent(object):
-    def __init__(self, bandits, train_test_split=0.1, num_trails = 10000, band_type = 'gaussian'):
+    def __init__(self, bandits, train_test_split=0.1, num_trails = 100000, band_type = 'gaussian'):
         
         if band_type=='gaussian':
             self.bandits = [GaussianBandit(bandit[0], bandit[1]) for bandit in bandits]
@@ -40,11 +40,16 @@ class Agent(object):
         self.reward_dict = {}
         self.n_trails = num_trails
             
-    def reset_agent(self):
+    def reset_agent(self, optimistic = False):
         self.total_reward=0
         self.N = np.zeros(self.n_ads)
-        self.Q = np.zeros(self.n_ads)
         self.avg_rewards=[]
+        
+        if optimistic == False:
+            self.Q = np.zeros(self.n_ads)
+            
+        else :
+            self.Q = np.ones(self.n_ads)*10
 
     def best_ad_random_actions(self):
         
@@ -73,6 +78,36 @@ class Agent(object):
             self.avg_rewards.append(avg_reward_so_far)
         
         df = pd.DataFrame(self.avg_rewards, columns = ['A/B/n'])
+        return df, best_ad_index
+    
+    def optimistic_agent(self, eps):
+        
+        self.reset_agent()
+        print("Agent is reset")
+        
+        ad_chosen = np.random.randint(self.n_ads)
+        
+        for i in range(self.n_trails):
+            R = self.bandits[ad_chosen].get_reward()
+            self.N[ad_chosen] += 1
+            self.Q[ad_chosen] += (1 / self.N[ad_chosen]) * (R - self.Q[ad_chosen])
+            self.total_reward += R
+            avg_reward_so_far = self.total_reward / (i + 1)
+            self.avg_rewards.append(avg_reward_so_far)
+            
+            if np.random.uniform()<= eps:
+                ad_chosen = np.random.randint(self.n_ads)
+            else:
+                ad_chosen = np.argmax(self.Q)
+        
+        best_ad_index = np.argmax(self.Q)
+        print(self.Q)
+        
+        print("After optimistic method the best performing ad is {}. Epsilon: {}".format(best_ad_index+1, eps))
+        
+        self.reward_dict[eps] = self.avg_rewards
+        
+        df= pd.DataFrame.from_dict(self.reward_dict)
         return df, best_ad_index
     
     def epsilon_greedy(self, eps):
@@ -122,19 +157,19 @@ class Agent(object):
 if __name__ =="__main__":
     
     # Gaussian Bandits
-    ads = [(3,2), (0,1), (-1,6), (1,4)]
+    # ads = [(3,2), (0,1), (-1,6), (1,4)]
     
     # Bernoulli Bandits
-    # ads = [0.004, 0.016, 0.2, 0.028, 0.031]
+    ads = [0.4, 0.016, 0.2, 0.028, 0.031]
     
-    eps_values = [0.01, 0.1, 0.2, 0.6]
+    eps_values = [0.01, 0.1, 0.2]
         
-    ads_initialized = Agent(ads, band_type="gaussian")
+    ads_initialized = Agent(ads, band_type="bernoulli")
     
-    # rewards, best_ad = ads_initialized.epsilon_greedy(0.01)
+    # rewards, best_ad = ads_initialized.optimistic_agent(eps_values[0])
     # ads_initialized.plot(rewards, mode = 'single epsilon')
     
     for eps in eps_values:
-        rewards, best_ad = ads_initialized.epsilon_greedy(eps)
+        rewards, best_ad = ads_initialized.optimistic_agent(eps)
         
     ads_initialized.plot(rewards, mode = 'multiple epsilons')
